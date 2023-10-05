b87012e8546fc224bfbae36fddb8c6cb9d4501c4
sun0405 <1367121328@qq.com>
2023-10-05 11:22:52 +0800

    补充了相关代码

--- note/exercise/DIY/Alist.java
+++ note/exercise/DIY/Alist.java
@@ -4,8 +4,8 @@
 
 public class AList {
     /** Creates an empty list. */
-    int[] items;
-    int size;
+    private int[] items;
+    private int size;
     public AList() {
         items = new int[100];
         size = 0;
@@ -13,26 +13,35 @@ public class AList {
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        if (size == items.length) {
+            int[] a = new int[size + 1];
+            System.arraycopy(items, 0, a, 0, size);
+            items = a;
+        }
 
+        items[size] = x;
+        size = size + 1;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;
+        return items[size - 1];
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;
+        return items[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;
+        return size;
     }
 
     /** Deletes item from back of the list and
      * returns deleted item. */
     public int removeLast() {
-        return 0;
+        int x = getLast();
+        size = size - 1;
+        return x;
     }
 }
