e9ff1582be3a21ecf6141053cc86600995277a28
sun0405 <1367121328@qq.com>
2023-10-08 11:05:30 +0800

    进行了一部分的初始化, 主要关于Node节点的初始化, 以及一些基本的方法的实现, 但是还有一些方法没有实现, 例如get, remove, size等等, 这些方法的实现需要用到一些其他的方法, 例如getRecursive, removeFirst, removeLast等等, 所以这些方法的实现需要等到其他方法实现之后再进行实现.

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -2,6 +2,28 @@
   创建一个双端队列, 并实现project1A中的功能
  */
 
-public class LinkedListDeque {
+public class LinkedListDeque<T> {
+    // create a node
+    private class Node{
+        private T first;
+        private Node next;
+        private Node prev;
+        private Node(T i, Node p, Node n) {
+            first = i;
+            next = n;
+            prev = p;
+        }
+    }
+
+    //size
+    private int size;
+    //create a sentinel node
+    private Node sentinel = new Node(null, null, null);
+
+    //create a empty list
+    public LinkedListDeque() {
+        size = 0;
+    }
+
 
 }
